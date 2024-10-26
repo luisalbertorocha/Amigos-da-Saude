7b2d59d764e4bd95fd647969d87a34f5a9576d61
Luis Alberto <luisalberto-rocha@hotmail.com>
2024-10-26 13:51:18 -0300

    update criar avatar fix

--- src/context/AvatarProvider.jsx
+++ src/context/AvatarProvider.jsx
@@ -1,4 +1,16 @@
 import { createContext, useEffect, useState } from "react";
+import homem1 from "../assets/avatar/pele/homem-1.svg";
+import homem2 from "../assets/avatar/pele/homem-2.svg";
+import homem3 from "../assets/avatar/pele/homem-3.svg";
+import mulher1 from "../assets/avatar/pele/mulher-1.svg";
+import mulher2 from "../assets/avatar/pele/mulher-2.svg";
+import mulher3 from "../assets/avatar/pele/mulher-3.svg";
+import camisa1 from "../assets/avatar/camisa/camisa-1.svg";
+import camisa2 from "../assets/avatar/camisa/camisa-2.svg";
+import calca1 from "../assets/avatar/calca/calca-1.svg";
+import calca2 from "../assets/avatar/calca/calca-2.svg";
+import tenis1 from "../assets/avatar/tenis/tenis-1.svg";
+import tenis2 from "../assets/avatar/tenis/tenis-2.svg";
 
 export const AvatarContext = createContext({});
 
@@ -8,27 +20,27 @@ export const AvatarProvider = ({ children }) => {
 
   const avatarInfo = {
     pele: [
-      "/src/assets/avatar/pele/homem-1.svg",
-      "/src/assets/avatar/pele/homem-2.svg",
-      "/src/assets/avatar/pele/homem-3.svg",
-      "/src/assets/avatar/pele/mulher-1.svg",
-      "/src/assets/avatar/pele/mulher-2.svg",
-      "/src/assets/avatar/pele/mulher-3.svg",
+      homem1,
+      homem2,
+      homem3,
+      mulher1,
+      mulher2,
+      mulher3,
     ],
     camisa: [
       "",
-      "/src/assets/avatar/camisa/camisa-1.svg",
-      "/src/assets/avatar/camisa/camisa-2.svg",
+      camisa1,
+      camisa2,
     ],
     calca: [
       "",
-      "/src/assets/avatar/calca/calca-1.svg",
-      "/src/assets/avatar/calca/calca-2.svg",
+      calca1,
+      calca2,
     ],
     tenis: [
       "",
-      "/src/assets/avatar/tenis/tenis-1.svg",
-      "/src/assets/avatar/tenis/tenis-2.svg"
+      tenis1,
+      tenis2,
     ],
   };
 
